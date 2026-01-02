d72cc362a9a276555eba823fd7fe59767963a2b0
cho45 <cho45@lowreal.net>
2026-01-02 17:20:59 +0900

    app: remove excessive blank lines

--- app.go
+++ app.go
@@ -44,37 +44,22 @@ func NewApp(config *Config, db *sql.DB, tfidfDB *sql.DB, workerDB *sql.DB, queue
 }
 
 func (app *App) RequireAuth(next echo.HandlerFunc) echo.HandlerFunc {
-
 	return func(c echo.Context) error {
-
 		if !app.IsAuth(c) {
-
 			if c.Request().Header.Get("X-Requested-With") == "XMLHttpRequest" ||
-
 				strings.HasPrefix(c.Request().URL.Path, "/api/") {
-
 				return echo.NewHTTPError(http.StatusUnauthorized, "Authentication required")
-
 			}
-
 			return c.Redirect(http.StatusFound, "/login?return="+c.Request().URL.Path)
-
 		}
-
 		return next(c)
-
 	}
-
 }
 
 func (app *App) IsAuth(c echo.Context) bool {
-
 	sess, _ := session.Get("session", c)
-
 	auth, ok := sess.Values["auth"].(bool)
-
 	return ok && auth
-
 }
 
 // postprocess はフォーマット済み HTML に対して postprocess を実行する
